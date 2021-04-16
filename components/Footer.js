8d92f299dbd44d3c9ce036c42e46a9ec90be7525
Brandon Leichty <brandonl@me.com>
2021-04-15 23:39:47 -0500

    add initial footer component

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+      <a href="https://twitter.com/brandonleichty" target="_blank" rel="noopener noreferrer">@brandonleichty</a>
+    </footer>
+  )
+}
+
+export default Footer
