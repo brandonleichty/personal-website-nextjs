3c37beb67300e6656ca3c0d7c3c77ab838943aa7
Brandon Leichty <brandonl@me.com>
2021-04-15 23:39:56 -0500

    add initial header component

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigation from './Navigation'
+
+function Header({heading}) {
+  return (
+    <header>
+      <Navigation />
+    </header>
+  )
+}
+
+export default Header
