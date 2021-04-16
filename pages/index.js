b44e0226dd73b4cbfaba11ea67fc8e84d6a9847e
Brandon Leichty <brandonl@me.com>
2021-04-15 23:42:41 -0500

    add script to check dark mode preference

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,24 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Brandon Leichty</title>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+              document.documentElement.classList.add('dark');
+            } else {
+              document.documentElement.classList.remove('dark');
+            }`,
+          }}
+        />
       </Head>
-
       <main>
-        <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <div className="heading">
+          <h1>Brandon Leichty</h1>
+        </div>
       </main>
-      <footer></footer>
-    </div>
+    </>
   );
 }
