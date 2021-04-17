a3832c1b568a4d4f83b439cc2c699bed5f233e2c
Brandon Leichty <brandonl@me.com>
2021-04-16 23:49:41 -0500

    add initial about page content

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Head from "next/head";
+import Image from 'next/image'
+
 
 export default function About() {
   return (
@@ -16,9 +18,39 @@ export default function About() {
           }}
         />
       </Head>
-      <main>
-        <div className="heading">
-          <h1>About</h1>
+      <main className="max-w-3xl mx-auto">
+        <div className="heading about">
+          <h1>About Brandon</h1>
+          <section className="text-lg sm:text-xl">
+            <p>In 1998 The Legend of Zelda: Ocarina of Time was released.</p>
+
+            <p>I was eleven.</p>
+
+            <p>
+              And that game is what started me down the path of computer
+              programming. I wanted to learn how to create games (like Zelda)
+              using a computer. I’ve dabbled in many languages since a young
+              age, such as C, C++, and Cocoa. And though those languages were
+              (and are) cool, there was an artistic side of me that wanted to
+              create things that worked outside the command line.
+            </p>
+
+            <p>
+              If we fast forward to the current year, I’ve now been working for
+              Apple over 11 years — and have spent the last four years learning
+              JavaScript. In 2019 I completed a full stack JavaScript Techdegree
+              through a company called Treehouse — where I learned things such
+              as React, NodeJS, AngularJS, Express, Firebase, MongoDB, and more.
+            </p>
+
+            <p>
+              One of the reasons I love JavaScript so much is because it’s
+              always changing. And learning new things is one of my biggest
+              passions. I’m a big reader (50 books in a year is my record) — And
+              there’s always something new to learn in the JavaScript/web world.
+              Whether it be a new framework, paradigm, or best practice.
+            </p>
+          </section>
         </div>
       </main>
     </>
