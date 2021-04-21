92bb85bbbced169abbf71ab314b1f8ce60e79c95
Brandon Leichty <brandonl@me.com>
2021-04-21 00:34:14 -0500

    add profile image

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,9 @@ export default function Home() {
       <main>
         <div className="heading landing">
           <div>
+            <div className="hero-image-wrapper">
+              <img src="images/brandon.png" className="rounded-full	border-white border-solid w-60 h-60"></img>
+            </div>
             <h1>Brandon Leichty</h1>
             <p className="main-subheading">
               Full Stack JavaScript developer and{" "}
