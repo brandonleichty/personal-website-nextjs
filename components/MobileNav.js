69ffa9137ee5b93ba920b3d2f5be172b46c225a9
Brandon Leichty <brandonl@me.com>
2021-04-23 18:32:30 -0500

    seperate mobile navigation into component

---
+++ components/MobileNav.js
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import Switch from "../components/Switch";
+import { useState } from "react";
+import Burger from "@animated-burgers/burger-squeeze";
+import "@animated-burgers/burger-squeeze/dist/styles.css";
+
+import React from "react";
+
+function MobileNav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="burger-nav-wrapper navigation-wrapper inline-flex sm:hidden">
+      <Burger
+        isOpen={isOpen}
+        Component="button"
+        type="button"
+        onClick={() => setIsOpen(!isOpen)}
+      />
+      <div className="mobile-navigation-wrapper">
+        <ul
+          className={`text-black dark:text-white ${isOpen ? "active" : null}`}
+        >
+          <li>
+            <Link href="/">
+              <a onClick={() => setIsOpen(false)}>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a onClick={() => setIsOpen(false)}>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/resources">
+              <a onClick={() => setIsOpen(false)}>Resources</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/uses">
+              <a onClick={() => setIsOpen(false)}>Uses</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog">
+              <a onClick={() => setIsOpen(false)}>Blog</a>
+            </Link>
+          </li>
+          <li>
+            <Switch className="theme-switch" />
+          </li>
+        </ul>
+      </div>
+      <div className={`overlay ${isOpen ? "active" : null}`} />
+    </nav>
+  );
+}
+
+export default MobileNav;
