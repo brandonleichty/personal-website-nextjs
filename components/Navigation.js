cda6e2ee21bd51545d912a385f963d41e8fc6af8
Brandon Leichty <brandonl@me.com>
2021-04-23 18:32:49 -0500

    remove mobile navigation and place in a seperate component

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,12 +1,8 @@
 import Switch from "../components/Switch";
 import Link from "next/link";
-import { useState } from "react";
-import Burger from "@animated-burgers/burger-squeeze";
-import "@animated-burgers/burger-squeeze/dist/styles.css";
+import MobileNav from "./MobileNav";
 
 function Navigation() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <>
       <nav className="navigation-wrapper hidden sm:inline-flex">
@@ -39,45 +35,7 @@ function Navigation() {
         </ul>
         <Switch className="theme-switch" />
       </nav>
-      <nav className="burger-nav-wrapper navigation-wrapper inline-flex sm:hidden">
-        <Burger
-          isOpen={isOpen}
-          Component="button"
-          type="button"
-          onClick={() => setIsOpen(!isOpen)}
-        />
-        <div className="mobile-navigation-wrapper">
-        <ul className={`text-black dark:text-white ${isOpen ? "opacity-100" : "opacity-0"}`}>
-          <li>
-            <Link href="/">
-              <a onClick={() => setIsOpen(false)}>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a onClick={() => setIsOpen(false)}>About</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/resources">
-              <a onClick={() => setIsOpen(false)}>Resources</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/uses">
-              <a onClick={() => setIsOpen(false)}>Uses</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/blog">
-              <a onClick={() => setIsOpen(false)}>Blog</a>
-            </Link>
-          </li>
-        <li><Switch className="theme-switch" /></li>
-        </ul>
-      </div>
-        <div className={`overlay ${isOpen ? "opacity-100" : "opacity-0"}`} />
-      </nav>
+      <MobileNav />
     </>
   );
 }
