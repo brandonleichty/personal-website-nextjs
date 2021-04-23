b856acfb59adfef859303d8cfffdccd7357efb90
Brandon Leichty <brandonl@me.com>
2021-04-22 23:01:08 -0500

    add burger menu

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,39 +1,48 @@
 import Switch from "../components/Switch";
 import Link from "next/link";
-import React from "react";
+import {useState} from "react";
+import Burger from '@animated-burgers/burger-squeeze' 
+import '@animated-burgers/burger-squeeze/dist/styles.css' 
 
 function Navigation() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <nav className="navigation-wrapper inline-flex">
-      <ul>
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/resources">
-            <a>Resources</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/uses">
-            <a>Uses</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/blog">
-            <a>Blog</a>
-          </Link>
-        </li>
-      </ul>
-      <Switch className="theme-switch"/>
-    </nav>
+    <>
+      <nav className="navigation-wrapper hidden sm:inline-flex">
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/resources">
+              <a>Resources</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/uses">
+              <a>Uses</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog">
+              <a>Blog</a>
+            </Link>
+          </li>
+        </ul>
+        <Switch className="theme-switch"/>
+      </nav>
+      <nav className="burger-nav-wrapper navigation-wrapper inline-flex sm:hidden">
+        <Burger isOpen={ isOpen } Component="button" type="button" onClick={()=>setIsOpen(!isOpen)} />
+      </nav>
+    </>
   );
 }
 
