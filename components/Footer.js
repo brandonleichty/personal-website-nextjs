daafe700c1913921ff02391d096ea610d90429b8
Brandon Leichty <brandonl@me.com>
2021-04-23 18:58:26 -0500

    update footer with new text and a emoji

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className="text-black dark:text-white">
-        <a href="https://twitter.com/brandonleichty" target="_blank" rel="noopener noreferrer">@brandonleichty</a>
+    <footer className="text-black dark:text-white font-Gilroy font-thin text-gray">
+        <p className="text-3xl">✌️</p>
+        <p className="pt-2">Made in Minneapolis</p>
+        <p className="text-sm">&copy; 2021</p>
+        {/* <a href="https://twitter.com/brandonleichty" target="_blank" rel="noopener noreferrer">@brandonleichty</a> */}
     </footer>
   )
 }
