b3aa1cd286378d03843366ea9539d342ee6aeeeb
Brandon Leichty <brandonl@me.com>
2021-04-30 21:41:53 -0500

    add desk image to "uses" page

--- pages/uses.js
+++ pages/uses.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Uses() {
   return (
@@ -10,6 +11,18 @@ export default function Uses() {
       <main>
         <div className="heading uses">
           <h1>Uses</h1>
+        </div>
+        <div className="desk-image-wrapper">
+          <div className="desk-image">
+            <Image
+              src="/images/desksetup.jpeg"
+              alt="Picture of Brandon's desk setup"
+              width={4032}
+              height={3024}
+              priority
+              className="rounded-lg"
+            />
+          </div>
         </div>
       </main>
     </>
