c377cd82ff8cb298d191d89c522cd09159d00e3a
Brandon Leichty <brandonl@me.com>
2021-05-01 23:35:46 -0500

    change layout of "uses" page and "resources" page

--- pages/resources.js
+++ pages/resources.js
@@ -10,10 +10,10 @@ export default function Resources() {
       <main>
         <div className="heading resources">
           <h1>Resources</h1>
-          <div class="max-w-4xl mx-auto grid-container grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 gap-y-16 text-center">
+          <div class="max-w-4xl mx-auto grid-container grid grid-flow-row grid-cols-1 sm:grid-cols-2 gap-24 gap-y-16 text-left">
         <div class="resource-section-heading">
-          <h3 class="mb-3 text-2xl font-medium text-purple dark:text-baby-blue">Tutorial Sites</h3>
-          <ul class="space-y-2 dark:text-favWhite">
+          <h3 class="mb-8 mt-16 text-4xl font-medium text-purple dark:text-baby-blue">Tutorial Sites</h3>
+          <ul class="space-y-6 dark:text-favWhite">
             <li><a href="https://www.leveluptutorials.com" target="_blank" rel="noopener noreferrer">LevelUp Tutorials</a></li>
             <li><a href="https://wesbos.com" target="_blank" rel="noopener noreferrer">Wes Bos</a></li>
             <li><a href="https://frontendmasters.com" target="_blank" rel="noopener noreferrer">FrontendMasters</a></li>
@@ -23,8 +23,8 @@ export default function Resources() {
           </ul>
         </div>
         <div class="resource-section-heading">
-          <h3 class=" mb-3  text-2xl font-medium text-purple dark:text-baby-blue">YouTube Channels</h3>
-          <ul class="space-y-2 dark:text-favWhite">
+          <h3 class="mb-8 mt-16 text-4xl font-medium text-purple dark:text-baby-blue">YouTube Channels</h3>
+          <ul class="space-y-6 dark:text-favWhite">
             <li><a href="https://www.youtube.com/channel/UCsBjURrPoezykLs9EqgamOA" target="_blank" rel="noopener noreferrer">fireship.io</a></li>
             <li><a href="https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw" target="_blank" rel="noopener noreferrer">WebDev Simplified</a></li>
             <li><a href="https://www.youtube.com/user/TechGuyWeb" target="_blank" rel="noopener noreferrer">Travesty Media</a></li>
@@ -34,16 +34,8 @@ export default function Resources() {
           </ul>
         </div>
         <div class="resource-section-heading">
-          <h3 class="mb-3 text-2xl font-medium text-purple dark:text-baby-blue">Design Inspiration</h3>
-          <ul class="space-y-2 dark:text-favWhite">
-            <li><a href="https://dribbble.com" target="_blank" rel="noopener noreferrer">Dribble</a></li>
-            <li><a href="https://klim.co.nz" target="_blank" rel="noopener noreferrer">Klim Type Foundry</a></li>
-            <li><a href="https://www.awwwards.com" target="_blank" rel="noopener noreferrer">Awwwards</a></li>
-          </ul>
-        </div>
-        <div class="resource-section-heading">
-          <h3 class="mb-3 text-2xl font-medium text-purple dark:text-baby-blue">Other Resources</h3>
-          <ul class="space-y-2 dark:text-favWhite">
+          <h3 class="mb-8 mt-16 text-4xl font-medium text-purple dark:text-baby-blue">Other Resources</h3>
+          <ul class="space-y-6 dark:text-favWhite">
             <li><a href="https://www.meetup.com/JavaScriptMN/" target="_blank" rel="noopener noreferrer">JavaScript MN Meetup</a></li>
             <li><a href="https://javascriptweekly.com" target="_blank" rel="noopener noreferrer">JavaScript Weekly</a></li>
             <li><a href="https://www.masterywithsql.com" target="_blank" rel="noopener noreferrer">Mastery with SQL</a></li>
@@ -54,8 +46,8 @@ export default function Resources() {
           </ul>
         </div>
         <div class="resource-section-heading">
-          <h3 class="mb-3 text-2xl font-medium text-purple dark:text-baby-blue">Books</h3>
-          <ul class="space-y-2 dark:text-favWhite">
+          <h3 class="mb-8 mt-16 text-4xl font-medium text-purple dark:text-baby-blue">Books</h3>
+          <ul class="space-y-6 dark:text-favWhite">
             <li><a href="https://eloquentjavascript.net" target="_blank" rel="noopener noreferrer">Eloquent JavaScript</a></li>
             <li><a href="https://github.com/getify/You-Dont-Know-JS" target="_blank" rel="noopener noreferrer">You Don't Know JS</a></li>
             <li><a href="https://nostarch.com/oojs" target="_blank" rel="noopener noreferrer">The Principles of<br/>Object-Oriented JavaScript</a></li>
@@ -69,14 +61,22 @@ export default function Resources() {
           </ul>
         </div>
         <div class="resource-section-heading">
-          <h3 class="mb-3 text-2xl font-medium text-purple dark:text-baby-blue">Apps & Tools</h3>
-          <ul class="space-y-2 dark:text-favWhite">      
+          <h3 class="mb-8 mt-16 text-4xl font-medium text-purple dark:text-baby-blue">Apps & Tools</h3>
+          <ul class="space-y-6 dark:text-favWhite">      
             <li><a href="https://code.visualstudio.com" target="_blank" rel="noopener noreferrer">VSCode</a></li>
             <li><a href="https://tableplus.com" target="_blank" rel="noopener noreferrer">Table Plus</a></li>
             <li><a href="https://insomnia.rest" target="_blank" rel="noopener noreferrer">Insomnia</a></li>
             <li><a href="https://runjs.app" target="_blank" rel="noopener noreferrer">RunJS</a></li>
             <li><a href="https://www.sketch.com" target="_blank" rel="noopener noreferrer">Sketch</a></li>
             <li><a href="https://www.figma.com" target="_blank" rel="noopener noreferrer">Figma</a></li>
+          </ul>
+        </div>
+        <div class="resource-section-heading">
+          <h3 class="mb-8 mt-16 text-4xl font-medium text-purple dark:text-baby-blue">Design Inspiration</h3>
+          <ul class="space-y-6 dark:text-favWhite">
+            <li><a href="https://dribbble.com" target="_blank" rel="noopener noreferrer">Dribble</a></li>
+            <li><a href="https://klim.co.nz" target="_blank" rel="noopener noreferrer">Klim Type Foundry</a></li>
+            <li><a href="https://www.awwwards.com" target="_blank" rel="noopener noreferrer">Awwwards</a></li>
           </ul>
         </div>
       </div>
--- pages/uses.js
+++ pages/uses.js
@@ -24,28 +24,28 @@ export default function Uses() {
         </div>
       </div>
       <main className="layout-wrapper">
-        <div class="temp-wrapper">
+        <div class="uses-wrapper">
           <div class="resource-section-heading">
-            <h3 class="mb-3 mt-10 text-4xl font-medium text-purple dark:text-baby-blue">
+            <h3 class="mb-8 mt-16 text-4xl font-medium text-purple dark:text-baby-blue">
               Desk Setup
             </h3>
-            <ul class="space-y-4 dark:text-favWhite">
+            <ul class="space-y-6 dark:text-favWhite">
               <li>
                 2019 16" MacBook Pro 2.3 GHz 8-Core Intel Core i9 with 64gb
                 memory
               </li>
               <li>Twelve South Curve - Matte White</li>
-              <li>IKEA BEKANT desk (white stained oak veneer)</li>
+              <li>IKEA BEKANT desk (white stained oak)</li>
               <li>Grovemade Wool Felt Desk Pad (light felt)</li>
               <li>Tomons Swing Arm Desk Lamp</li>
               <li>Logitech MX Master 2S Mouse</li>
             </ul>
           </div>
           <div class="resource-section-heading">
-            <h3 class=" mb-3 mt-10 text-4xl font-medium text-purple dark:text-baby-blue">
+            <h3 class="mb-8 mt-16 text-4xl font-medium text-purple dark:text-baby-blue">
               Desktop Apps
             </h3>
-            <ul class="space-y-4 dark:text-favWhite">
+            <ul class="space-y-6 dark:text-favWhite">
               <li>Table Plus</li>
               <li>RunJS</li>
               <li>Sketch</li>
@@ -53,12 +53,13 @@ export default function Uses() {
             </ul>
           </div>
           <div class="resource-section-heading">
-            <h3 class=" mb-3 mt-10 text-4xl font-medium text-purple dark:text-baby-blue">
+            <h3 class="mb-8 mt-16 text-4xl font-medium text-purple dark:text-baby-blue">
               Editor + Terminal
             </h3>
-            <ul class="space-y-4 dark:text-favWhite">
+            <ul class="space-y-6 dark:text-favWhite">
               <li>VS Code</li>
               <li>Font: Fira Code</li>
+              <li>Theme: Retreon</li>
             </ul>
           </div>
         </div>
