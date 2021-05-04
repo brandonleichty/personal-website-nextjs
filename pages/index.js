53c0f1cf72b3968f51cd0050129b002c5b1a10b2
Brandon Leichty <brandonl@me.com>
2021-05-03 22:57:30 -0500

    add transparent profile photo with gradient background

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,10 @@ export default function Home() {
         <section>
           <div className="heading landing">
             <div>
-              {/* <div className="hero-image-wrapper">
+              <div className="hero-image-wrapper">
                 <div className="hero-image">
                   <Image
-                    src="/images/brandonMin.png"
+                    src="/images/brandon-transparent.png"
                     alt="Picture of the author"
                     width={300}
                     height={300}
@@ -32,7 +32,7 @@ export default function Home() {
                     className="hero-img rounded-full border-white border-solid w-48 h-48 sm:w-60 sm:h-60"
                   />
                 </div>
-              </div> */}
+              </div>
               <h1>Brandon Leichty</h1>
               <p className="main-subheading">
                 Full Stack JavaScript developer from Minneapolis.
