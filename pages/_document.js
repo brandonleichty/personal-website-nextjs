4a39a66387a4da9df181f202dd40b042732ffeed
Brandon Leichty <brandonl@me.com>
2021-05-04 00:40:23 -0500

    add OG Image to head

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   // static async getInitialProps(ctx) {
@@ -9,14 +9,22 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta
+            property="og:image"
+            content="https://www.brandonleichty.com/images/og-default-purple-blue.png"
+            key="ogimage"
+          />
+          <meta property="og:image:width" content="1280" key="ogimagewidth"/>
+          <meta property="og:image:height" content="675" key="ogimageheight"/>
+        </Head>
         <body className="bg-white dark:bg-dark-navy-2 text-almost-black dark:text-white transition-colors">
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
