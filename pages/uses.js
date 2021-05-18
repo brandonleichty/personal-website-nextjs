94f54757d03b32cdc012601e7418bf41339e701a
Brandon Leichty <brandonl@me.com>
2021-05-17 23:49:32 -0500

    update list of gear and apps

--- pages/uses.js
+++ pages/uses.js
@@ -30,14 +30,29 @@ export default function Uses() {
               Desk Setup
             </h3>
             <ul class="space-y-6 dark:text-favWhite">
+              <li>Twelve South Curve - Matte White</li>
+              <li>IKEA BEKANT desk (white stained oak)</li>
+              <li>IKEA HATTEFJÄLL office chair (Gunnared beige)</li>
+              <li>Grovemade Wool Felt Desk Pad (light felt)</li>
+              <li>Tomons Swing Arm Desk Lamp</li>
+              <li>Muir Way Minnesota Elevation Map</li>
+            </ul>
+          </div>
+          <div class="resource-section-heading">
+            <h3 class="mb-8 mt-16 text-5xl font-medium text-purple dark:text-baby-blue">
+              Hardware
+            </h3>
+            <ul class="space-y-6 dark:text-favWhite">
               <li>
                 2019 16" MacBook Pro 2.3 GHz 8-Core Intel Core i9 with 64gb
                 memory
               </li>
-              <li>Twelve South Curve - Matte White</li>
-              <li>IKEA BEKANT desk (white stained oak)</li>
-              <li>Grovemade Wool Felt Desk Pad (light felt)</li>
-              <li>Tomons Swing Arm Desk Lamp</li>
+              <li>
+                2021 24" M1 iMac (silver)
+              </li>
+              <li>
+                iPhone 12 Pro (silver)
+              </li>
               <li>Logitech MX Master 2S Mouse</li>
             </ul>
           </div>
@@ -46,10 +61,15 @@ export default function Uses() {
               Desktop Apps
             </h3>
             <ul class="space-y-6 dark:text-favWhite">
-              <li>Table Plus</li>
-              <li>RunJS</li>
-              <li>Sketch</li>
               <li>Things</li>
+              <li>Notion</li>
+              <li>Table Plus</li>
+              <li>Insomnia</li>
+              <li>RunJS</li>
+              <li>Sip</li>
+              <li>Cinema 4D</li>
+              <li>Figma</li>
+              <li>Sketch</li>
             </ul>
           </div>
           <div class="resource-section-heading">
@@ -58,6 +78,7 @@ export default function Uses() {
             </h3>
             <ul class="space-y-6 dark:text-favWhite">
               <li>VS Code</li>
+              <li>Hyper or iTerm</li>
               <li>Font: Fira Code</li>
               <li>Theme: Retreon</li>
             </ul>
