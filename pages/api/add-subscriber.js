318ab27841ca3f8485cf7f6a7b36df1a398bce70
Brandon Leichty <brandonl@me.com>
2021-05-27 13:23:29 -0500

    refactor with proper error messages

--- pages/api/add-subscriber.js
+++ pages/api/add-subscriber.js
@@ -4,33 +4,26 @@ const { format } = require('date-fns');
 const { Client } = require("@notionhq/client")
 const fetch = require('node-fetch');
 
-// Initializing a client
+
+// Initializing a Notion client
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 });
 
-
 // Serverless functions takes a request that has 1. an email adddress, and 2. the subscribers name (optional).
 export default async (req, res) => {
-  const { email, name } = JSON.parse(req.body);
+  const { email, name } = req.body;
   try {
-    const buttonDownResponse = await addButtonDownSubscriber(email, name);
-
-    const results = await buttonDownResponse.json();
-
-    if (buttonDownResponse.status == "201") {
-      res.status(201).json(results);
-    } else {
-      res.status(buttonDownResponse.status).json(results);
-    }
+    const result = await addButtonDownSubscriber(email, name)
+    const { status } = result;
+    res.status(status).json(result);
   } catch (error) {
     res.status(500).json({ error });
   }
-};
+}
 
 
 async function addButtonDownSubscriber(email, name = "") {
-  // This is specially added for the "note" feature of Buttondown. If a subscriber name is provided, a note/string will be added that reads "Name: John Doe"
   var notes = name ? `Name: ${name}` : "";
 
   try {
@@ -50,15 +43,47 @@ async function addButtonDownSubscriber(email, name = "") {
       }
     )
 
-    // If a subscriber is successfully added to Buttondown, add them to a Notino database
-    if (buttonDownResponse.status == "201") {
+
+    if (buttonDownResponse.status == 201 || buttonDownResponse.status == 200) {
       await addSubscriberToNotionDatabase(email, name);
+      return {
+        status: buttonDownResponse.status,
+        message: "Success! You've subscribed!",
+        email,
+        name
+      }
+    } else {
+      const errorMessage = await buttonDownResponse.json()
+      const message = parseButtondownErrorMessage(errorMessage)
+      return {
+        status: buttonDownResponse.status,
+        message,
+        email,
+        name
+      }
     }
-    // Return the entire Buttondown response object
-    return buttonDownResponse;
   } catch (error) {
     console.error(error)
   }
+}
+
+function parseButtondownErrorMessage(errorMessage) {
+  const duplcateEmailSubscriber = "That email address"
+  const spammyEmail = "Sorry, our system has previously detected this email"
+  const blankEmail = "This field may not be blank"
+
+
+  if (errorMessage[0]?.toLowerCase().includes(duplcateEmailSubscriber.toLowerCase())) {
+    return "The email address you provided is already subscribed."
+  }
+  if (errorMessage[0]?.toLowerCase().includes(spammyEmail.toLowerCase())) {
+    return "There seems to be an issue with the address your provided."
+  }
+  if (errorMessage?.email[0]?.toLowerCase().includes(blankEmail.toLowerCase())) {
+    return "Oops! No email address was provided."
+  }
+
+  return "Oh no! An unknown error occurred."
 }
 
 
@@ -66,7 +91,6 @@ async function addSubscriberToNotionDatabase(email, name = "") {
   const today = format(new Date(), 'yyyy-MM-dd');
 
   try {
-    // Add a new subscriber to a Notion database that includes an email, date added, and optional name
     const newSubscriber = await notion.pages.create({
       "parent": {
         "database_id": process.env.NOTION_DATABASE_ID
@@ -97,3 +121,97 @@ async function addSubscriberToNotionDatabase(email, name = "") {
     console.error(error)
   }
 }
+
+// // Initializing a client
+// const notion = new Client({
+//   auth: process.env.NOTION_TOKEN,
+// });
+
+
+// // Serverless functions takes a request that has 1. an email adddress, and 2. the subscribers name (optional).
+// export default async (req, res) => {
+//   const { email, name } = JSON.parse(req.body);
+//   try {
+//     const buttonDownResponse = await addButtonDownSubscriber(email, name);
+
+//     const results = await buttonDownResponse.json();
+
+//     if (buttonDownResponse.status == "201") {
+//       res.status(201).json(results);
+//     } else {
+//       res.status(buttonDownResponse.status).json(results);
+//     }
+//   } catch (error) {
+//     res.status(500).json({ error });
+//   }
+// };
+
+
+// async function addButtonDownSubscriber(email, name = "") {
+//   // This is specially added for the "note" feature of Buttondown. If a subscriber name is provided, a note/string will be added that reads "Name: John Doe"
+//   var notes = name ? `Name: ${name}` : "";
+
+//   try {
+//     const buttonDownResponse = await fetch(
+//       "https://api.buttondown.email/v1/subscribers",
+//       {
+//         method: "POST",
+//         headers: {
+//           Accept: "application/json",
+//           "Content-Type": "application/json",
+//           Authorization: "Token " + process.env.BUTTONDOWN_API_KEY,
+//         },
+//         body: JSON.stringify({
+//           email,
+//           notes
+//         })
+//       }
+//     )
+
+//     // If a subscriber is successfully added to Buttondown, add them to a Notino database
+//     if (buttonDownResponse.status == "201") {
+//       await addSubscriberToNotionDatabase(email, name);
+//     }
+//     // Return the entire Buttondown response object
+//     return buttonDownResponse;
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
+
+
+// async function addSubscriberToNotionDatabase(email, name = "") {
+//   const today = format(new Date(), 'yyyy-MM-dd');
+
+//   try {
+//     // Add a new subscriber to a Notion database that includes an email, date added, and optional name
+//     const newSubscriber = await notion.pages.create({
+//       "parent": {
+//         "database_id": process.env.NOTION_DATABASE_ID
+//       },
+//       "properties": {
+//         "Email": {
+//           "email": email
+//         },
+//         "Date added": {
+//           "type": "date",
+//           "date": {
+//             "start": today
+//           }
+//         },
+//         "Name": {
+//           "title": [
+//             {
+//               "text": {
+//                 "content": name
+//               }
+//             }
+//           ]
+//         }
+//       }
+//     })
+//     return newSubscriber;
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
