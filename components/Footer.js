1c19ac0cb87bd574475fc469ab981f31d47cfd09
Brandon Leichty <brandonl@me.com>
2021-05-27 13:23:45 -0500

    add state handling for processing and errors

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import SubscribeForm from './SubscribeForm'
+import { useState } from 'react';
 
 function Footer() {
+  const [loading, setLoading] = useState(false)
+  const [formMessage, setFormMessage] = useState("Sign up for my newsletter!")
+
   return (
     <footer className="text-black dark:text-white font-Gilroy font-thin text-gray">
-        <div className="email-form-text mb-5 text-center">
-          <p>Sign up for my newsletter!</p>
-          {/* <p>Stay up to date with me.</p> */}
-        </div>
-        <SubscribeForm />
-        <p className="text-3xl">✌️</p>
-        <p className="pt-2">Made in Minneapolis</p>
-        <p className="text-sm">&copy; 2021</p>
-        {/* <a href="https://twitter.com/brandonleichty" target="_blank" rel="noopener noreferrer">@brandonleichty</a> */}
+      <div className="email-form-text mb-5 text-center">
+        {loading ? <p>Processing...</p> : <p>{formMessage}</p>}
+      </div>
+      <SubscribeForm loading={loading} setLoading={setLoading} setFormMessage={setFormMessage} />
+      <p className="text-3xl">✌️</p>
+      <p className="pt-2">Made in Minneapolis</p>
+      <p className="text-sm">&copy; 2021</p>
+      {/* <a href="https://twitter.com/brandonleichty" target="_blank" rel="noopener noreferrer">@brandonleichty</a> */}
     </footer>
   )
 }
--- components/SubscribeForm.js
+++ components/SubscribeForm.js
@@ -1,17 +1,32 @@
 import React from "react";
 
-function SubscribeForm() {
+function SubscribeForm(props) {
+  const { loading, setLoading, setFormMessage } = props;
+
+
   const registerUser = async (event) => {
+    setLoading(true);
     event.preventDefault();
 
     const res = await fetch("/api/add-subscriber", {
       method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
       body: JSON.stringify({
         email: event.target.email.value,
       }),
     });
-
-    console.log(res)
+    const jsonResponseData = await res.json()
+    const { status, message } = jsonResponseData;
+    if (status != 200 || status != 201) {
+      setFormMessage(message)
+      setTimeout(() => setFormMessage("Sign up for my newsletter!"), 5000);
+    } else {
+      setFormMessage("Sign up for my newsletter!")
+    }
+    setLoading(false);
   };
   return (
     <form
