7a008cbe0a6bd80cab614c93daa6da95b623c6d8
Brandon Leichty <brandonl@me.com>
2021-05-28 22:54:19 -0500

    move state into SubscribeFrom component

--- components/Footer.js
+++ components/Footer.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import SubscribeForm from './SubscribeForm'
-import { useState } from 'react';
 
 function Footer() {
-  const [loading, setLoading] = useState(false)
-  const [formMessage, setFormMessage] = useState("Sign up for my newsletter!")
+
 
   return (
     <footer className="text-black dark:text-white font-Gilroy font-thin text-gray">
-      <div className="email-form-text mb-5 text-center">
-        {loading ? <p>Processing...</p> : <p>{formMessage}</p>}
-      </div>
-      <SubscribeForm loading={loading} setLoading={setLoading} setFormMessage={setFormMessage} />
+
+      <SubscribeForm />
       <p className="text-3xl">✌️</p>
       <p className="pt-2">Made in Minneapolis</p>
       <p className="text-sm">&copy; 2021</p>
